fbebbdea78cc09856ccf5395a0e62a92400589e9
abhii131 <25bec003@smvdu.ac.in>
2026-06-22 16:17:33 +0530

    Updated project

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ let registeredEvents =
 JSON.parse(localStorage.getItem("registeredEvents"))
 || [];
 
+/* Check duplicate registration */
+
+if(registeredEvents.includes(eventName)){
+    alert("You have already registered for this event!");
+    return;
+}
+
+/* Save registration */
+
 registeredEvents.push(eventName);
 
 localStorage.setItem(
-"registeredEvents",
-JSON.stringify(registeredEvents)
+    "registeredEvents",
+    JSON.stringify(registeredEvents)
 );
 
 alert("Successfully Registered!");
