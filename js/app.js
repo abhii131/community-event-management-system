526c71f7da4ac47844a4254609c2b254c328c895
abhii131 <25bec003@smvdu.ac.in>
2026-07-09 11:55:58 +0530

    Updated Community Event Management System

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
-function registerEvent(eventName){
+function registerEvent(eventName) {
 
-let registeredEvents =
-JSON.parse(localStorage.getItem("registeredEvents"))
-|| [];
+    // Check if user is logged in
+    let loggedIn = localStorage.getItem("loggedIn");
 
-/* Check duplicate registration */
+    if (!loggedIn) {
+        alert("Please login first!");
+        window.location.href = "login.html";
+        return;
+    }
 
-if(registeredEvents.includes(eventName)){
-    alert("You have already registered for this event!");
-    return;
-}
+    let registeredEvents =
+        JSON.parse(localStorage.getItem("registeredEvents")) || [];
 
-/* Save registration */
+    // Prevent duplicate registration
+    if (registeredEvents.includes(eventName)) {
+        alert("You have already registered for this event!");
+        return;
+    }
 
-registeredEvents.push(eventName);
+    // Save registration
+    registeredEvents.push(eventName);
 
-localStorage.setItem(
-    "registeredEvents",
-    JSON.stringify(registeredEvents)
-);
+    localStorage.setItem(
+        "registeredEvents",
+        JSON.stringify(registeredEvents)
+    );
 
-alert("Successfully Registered!");
+    alert("Successfully Registered for " + eventName + "!");
 
 }
